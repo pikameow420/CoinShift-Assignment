7f069a9dbc938b7c4f535416abe33289018d8aca
pikameow420 <lci2021045@iiitl.ac.in>
2024-07-10 05:08:45 +0530

    fun

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,33 +18,33 @@ import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { ethers } from "ethers";
 import { toast } from "react-toastify";
 import { useCreateSmartWallet } from "./createSmartWallet";
-
+ 
 const Home: React.FC = () => {
+  const { walletConnector, primaryWallet } = useDynamicContext();
   const onramper_key = import.meta.env.VITE_APP_ONRAMPER;
-
+ 
   // const { isWidgetOpen } = useContext(OnRamperWidget);
   const [onboardingStep, setOnboardingStep] = useState<number>(1);
   const { createSafeWallet } = useCreateSafeWallet();
   const { isWidgetOpen } = useContext(WidgetContext);
-  const {createSmartWallet} = useCreateSmartWallet()
-  
-
-
+ 
+ 
+ 
   const handleStepAdvance = () => {
     setOnboardingStep(onboardingStep + 1);
   };
-
+ 
   const handleCreateSafeWallet = async () => {
-    const safeAddr = await createSmartWallet();
+    const safeAddr = await useCreateSmartWallet();
     if (safeAddr) {
       setOnboardingStep(onboardingStep + 1);
     } else {
       console.error("Safe deployment was rejected or failed");
     }
   };
-
+ 
   const handleCreateSmartWallet = async () => {
-      const walletAddress = await useCreateSmartWallet()
+      const walletAddress = await useCreateSmartWallet(walletConnector, primaryWallet)
      if(walletAddress){
        console.log('Smart wallet created at address:', walletAddress);
      } 
@@ -52,15 +52,14 @@ const Home: React.FC = () => {
       console.error('Error creating smart wallet:');
     }
   } 
-
+ 
   const safeAddress = "0xaddress";
-
+ 
   const Step1Props = { handleStepAdvance };
   const Step2Props = { handleCreateSafeWallet };
   const Step3Props = { safeAddress };
-
-  const { walletConnector, primaryWallet } = useDynamicContext();
-
+ 
+ 
   // const getSigner = async (): Promise<ethers.Signer | null> => {
   //   try {
   //     return await primaryWallet?.connector?.ethers?.getSigner() as ethers.Signer;
@@ -69,7 +68,7 @@ const Home: React.FC = () => {
   //     return null;
   //   }
   // };
-
+ 
   // const getProvider = async (): Promise<ethers.Provider | null> => {
   //   try {
   //     const provider = await primaryWallet?.connector?.ethers?.getRpcProvider() as ethers.Provider
@@ -80,54 +79,54 @@ const Home: React.FC = () => {
   //     return null;
   //   }
   // };
-
+ 
 //   const Meow = async () => {
-
+ 
 //     const factoryAddress = '0x0BA5ED0c6AA8c49038F819E587E2633c4A9F428a';
 //     const factoryAbi = [
 //       "function createAccount(bytes[] calldata owners, uint256 nonce) external payable returns (address account)"
 //   ];
-
+ 
 //   const signer = (await walletConnector?.ethers?.getSigner()) as ethers.Signer;
 //   const factoryContract = new ethers.Contract(factoryAddress, factoryAbi, signer);
-
-
+ 
+ 
 //   const createSmartWallet = async () => {
 //     // Owner address (this could be the signer's address)
 //     const ownerAddress = primaryWallet?.address;
-
+ 
 //     // Encode the owner address as bytes
 //     const abiCoder = new ethers.AbiCoder();
-
+ 
 //     const owners = [abiCoder.encode(['address'], [ownerAddress])];
-
+ 
 //     // Define a nonce (should be unique)
 //     const nonce = 1;
-
+ 
 //     // Estimate gas for the transaction
 //     // const gasEstimate = await factoryContract.estimateGas.createAccount(owners, nonce, {
 //     //     value: ethers.parseEther('0') // Example value to send with the transaction
 //     // });
-
+ 
 //     console.log('Sending transaction...');
-
+ 
 //     // Execute the transaction
 //     const tx = await factoryContract.createAccount(owners, nonce, {
 //         value: ethers.parseEther('0'), 
 //         gasLimit: "5000000"
 //     });
-
+ 
 //     console.log('Transaction hash:', tx.hash);
-
+ 
 //     // Wait for the transaction to be mined
 //     const receipt = await tx.wait();
 //     console.log('Transaction confirmed in block', receipt.blockNumber);
 //     console.log('New smart wallet address:', receipt.events[0].args.account);
 // }
-
+ 
 // };
-
-
+ 
+ 
   return (
     <div className="app-container flex flex-row flex-wrap justify-center items-center gap-8 m-auto mt-20">
       <div>{isWidgetOpen && <OnRamperWidget />}</div>
@@ -152,7 +151,7 @@ const Home: React.FC = () => {
           </AccordionItem> */}
         {/* </Accordion> */}
       </div>
-
+ 
       <div>
         {/* {onboardingStep === 1 && <Step1 {...Step1Props} />}
         {onboardingStep === 2 && <Step2 {...Step2Props} />}
@@ -163,11 +162,11 @@ const Home: React.FC = () => {
             <Button onPress={handleCreateSmartWallet}>Create Coinbase Wallet</Button>
           </CardFooter>
         </Card>
-
+ 
         {/* {onboardingStep === 4 && <Step4 />} */}
       </div>
     </div>
   );
 };
-
+ 
 export default Home;
--- src/pages/createSmartWallet.tsx
+++ src/pages/createSmartWallet.tsx
@@ -1,53 +1,53 @@
-import { useDynamicContext } from '@dynamic-labs/sdk-react-core';
 import { ethers } from 'ethers';
 import React from 'react'
-
-const useCreateSmartWallet = async () => {
-    const { walletConnector, primaryWallet } = useDynamicContext();
-
+ 
+const useCreateSmartWallet = async (walletConnector:any, primaryWallet:any) => {
+ 
+ 
     const factoryAddress = '0x0BA5ED0c6AA8c49038F819E587E2633c4A9F428a';
     const factoryAbi = [
-      "function createAccount(bytes[] calldata owners, uint256 nonce) external payable returns (address account)"
+      "function createAccount(bytes[] calldata owners, uint256 nonce) external payable returns (address account)",
+      "function getAddress(bytes[] calldata owners, uint256 nonce) external view returns (address)"
   ];
-
+ 
   const signer = (await walletConnector?.ethers?.getSigner()) as ethers.Signer;
   const factoryContract = new ethers.Contract(factoryAddress, factoryAbi, signer);
-
-  const createSmartWallet = async () => {
-    // Owner address (this could be the signer's address)
-
-    const ownerAddress = primaryWallet?.address;
-
-    // Encode the owner address as bytes
-    const abiCoder = new ethers.AbiCoder();
-
-    const owners = [abiCoder.encode(['address'], [ownerAddress])];
-
-    // Define a nonce (should be unique)
-    const nonce = 1;
-
-    // Estimate gas for the transaction
-    // const gasEstimate = await factoryContract.estimateGas.createAccount(owners, nonce, {
-    //     value: ethers.parseEther('0') // Example value to send with the transaction
-    // });
-
-    console.log('Sending transaction...');
-
-    // Execute the transaction
-    const tx = await factoryContract.createAccount(owners, nonce, {
-        value: ethers.parseEther('0'), 
-        gasLimit: "5000000"
-    });
-
-    console.log('Transaction hash:', tx.hash);
-
-    // Wait for the transaction to be mined
-    const receipt = await tx.wait();
-    console.log('Transaction confirmed in block', receipt.blockNumber);
-    console.log('New smart wallet address:', receipt.events[0].args.account);
-    return receipt;
+await createSmartWallet(primaryWallet, factoryContract)
+ 
 }
-
+const createSmartWallet = async (primaryWallet:any, factoryContract:any) => {
+  // Owner address (this could be the signer's address)
+ 
+  const ownerAddress = primaryWallet?.address;
+ 
+  // Encode the owner address as bytes
+  const abiCoder = new ethers.AbiCoder();
+ 
+  const owners = [abiCoder.encode(['address'], [ownerAddress])];
+ 
+  // Define a nonce (should be unique)
+  const nonce = "0x0";
+ 
+  // Estimate gas for the transaction
+  // const gasEstimate = await factoryContract.estimateGas.createAccount(owners, nonce, {
+  //     value: ethers.parseEther('0') // Example value to send with the transaction
+  // });
+  const address = await factoryContract.getAddress(owners, nonce);
+  console.log('Predicted smart wallet address:', address);
+ 
+  console.log('Sending transaction...');
+ 
+  // Execute the transaction
+  
+  const tx = await factoryContract.createAccount(owners, nonce, {
+      value: ethers.parseEther('0'), 
+      gasLimit: "500000"
+  });
+ 
+  console.log('Transaction hash:', tx.hash);
+ 
+  // Wait for the transaction to be mined
+  const receipt = await tx.wait();
+  return address;
 }
 export {useCreateSmartWallet};
-
